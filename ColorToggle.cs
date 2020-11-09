6a2912b24352f5149b6b445ccb39148e1f7c1e40
Abd Majeed Abd Mumuni <38582706+BitsAndWits@users.noreply.github.com>
2020-11-09 11:22:31 +0100

    Add files via upload

---
+++ ColorToggle.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorToggle : MonoBehaviour
+{
+    public void ToggleColor()
+    {
+        Image img = gameObject.GetComponent<Image>();
+        if (img.color == Color.red)
+        {
+            img.color = Color.white;
+        } else
+        {
+            img.color = Color.red;
+        }
+    }
+}
---
+++ PrimeShields.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PrimeShields : MonoBehaviour
+{
+    public Image mainShield;
+    public Image[] shieldCells;
+    public GameObject TextComplete;
+
+    void Start()
+    {
+        Randomizer();
+    }
+
+    void Randomizer()
+    {
+        List<int> randomCells = new List<int>();
+        int number;
+
+        for (int i = 0; i < Random.Range(2, 7); i++)
+        {
+            do
+            {
+                number = Random.Range(1, 7);
+            } while (randomCells.Contains(number));
+            randomCells.Add(number);
+        }
+
+        for (int i = 0; i < randomCells.Count; i++)
+        {
+            shieldCells[randomCells[i]].color = Color.red;
+        }
+        mainShield.color = new Color(1, 0, 0, 0.6f);
+    }
+
+    public void CheckShield()
+    {
+        for (int i = 0; i < shieldCells.Length; i++)
+        {
+            if (shieldCells[i].color == Color.red)
+            {
+                break;
+            }
+
+            if (i == shieldCells.Length - 1)
+            {
+                TextComplete.SetActive(true);
+                mainShield.color = new Color(1, 1, 1, 0.6f);
+            }
+        }
+    }
+}
